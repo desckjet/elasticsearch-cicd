ba86946e9fb565dc9eebfde25b1b6a253ea2b4f4
david acevedo <desckjetd@gmail.com>
2019-09-05 16:00:33 -0500

    l

--- jenkins/build/gradle.sh
+++ jenkins/build/gradle.sh
@@ -5,4 +5,4 @@ echo "* Building jar!*"
 echo "****************"
 
 PROJ=/home/davidacevedo/repositories/elasticsearch-cicd/jenkins_home/workspace
-docker run --rm -v /root/.gradle:/root/.gradle -v $PROJ/ramp_up:/app -w /app gradle "$@"
+docker run --rm -v /root/.gradle:/root/.gradle -v $PROJ/$JOB_NAME:/app -w /app gradle "$@"
