75ab8eb00e1e88f717c12b2bf582e7cc6961e064
david acevedo <desckjetd@gmail.com>
2019-09-06 10:25:05 -0500

    add environment variables

--- jenkins/build/gradle.sh
+++ jenkins/build/gradle.sh
@@ -6,4 +6,5 @@ echo "****************"
 
 PROJ=/home/davidacevedo/repositories/elasticsearch-cicd/jenkins_home/workspace
 PROJ2=/home/davidacevedo/repositories/elasticsearch-cicd/pipeline/pipeline/elasticsearch-cicd/jenkins_home/workspace
+PROJ3=../../jenkins_home/workspace/
 docker run --rm -v /root/.gradle:/root/.gradle -v $ROUTE/$JOB_NAME:/app -w /app gradle "$@"
